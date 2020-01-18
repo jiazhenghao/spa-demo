44cbb1ac7587aa7bd36113df7cb0ebd8aac76961
Zhenghao Jia <cherie-xia@hotmail.com>
2020-01-18 21:42:01 +0800

    api

--- src/theme/Login.vue
+++ src/theme/Login.vue
@@ -1,40 +1,60 @@
 <template>
   <div class="content">
-    <h2>Login</h2>
-    <div class="field is-horizontal">
-      <div class="field-label is-normal">
-        <label class="label">Username</label>
-      </div>
-      <div class="field-body">
-        <div class="field">
-          <div class="control">
-            <input class="input" type="text" placeholder="Your username" />
+    <div v-if="isAuthenticated">
+      hello authenticated User!
+      <p>Name: {{ profile.firstName }}</p>
+      <p>Favorite Sandwich: {{ profile.favoriteSandwich }}</p>
+      <button v-on:click="logout()" class="button is-primary">
+        Logout
+      </button>
+    </div>
+    <div v-else>
+      <h2>Login</h2>
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">Username</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <input
+                class="input"
+                v-model="username"
+                type="text"
+                placeholder="Your username"
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="field is-horizontal">
-      <div class="field-label is-normal">
-        <label class="label">Password</label>
-      </div>
-      <div class="field-body">
-        <div class="field">
-          <div class="control">
-            <input class="input" type="password" placeholder="Your password" />
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">Password</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <input
+                class="input"
+                v-model="password"
+                type="password"
+                placeholder="Your password"
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="field is-horizontal">
-      <div class="field-label">
-        <!-- Left empty for spacing -->
-      </div>
-      <div class="field-body">
-        <div class="field">
-          <div class="control">
-            <button class="button is-primary">
-              Login
-            </button>
+      <div class="field is-horizontal">
+        <div class="field-label">
+          <!-- Left empty for spacing -->
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <button @click="login" class="button is-primary">
+                Login
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -43,7 +63,54 @@
 </template>
 
 <script>
-export default {};
+import appService from "../app.service";
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+      isAuthenticated: false,
+      profile: {}
+    };
+  },
+  watch: {
+    isAuthenticated(newValue, oldValue) {
+      if (newValue) {
+        appService.getProfile().then(profile => {
+          this.profile = profile;
+        });
+      } else {
+        this.profile = {};
+      }
+    }
+  },
+  methods: {
+    login() {
+      appService
+        .login({ username: this.username, password: this.password })
+        .then(data => {
+          window.localStorage.setItem("token", data.token);
+          window.localStorage.setItem("tokenExpiration", data.expiration);
+          this.isAuthenticated = true;
+          this.username = "";
+          this.password = "";
+        })
+        .catch(() => window.alert("Could not login!"));
+    },
+    logout() {
+      window.localStorage.setItem("token", null);
+      window.localStorage.setItem("tokenExpiration", null);
+      this.isAuthenticated = false;
+    }
+  },
+  created() {
+    let expiration = window.localStorage.getItem("tokenExpiration");
+    let unixTimestamp = new Date().getTime() / 1000;
+    if (expiration !== null && parseInt(expiration) - unixTimestamp > 0) {
+      this.isAuthenticated = true;
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
